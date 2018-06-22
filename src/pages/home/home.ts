b834481bbc50194cfec983e4ef8d3aa4675c6e29
rafael <rafael.r.acosta.m.93@gmail.com>
2018-06-22 17:08:26 -0400

    Primer commit

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { NavController } from 'ionic-angular';
+import { FormBuilder, Validators, FormGroup } from '@angular/forms';
+import { NavController, AlertController } from 'ionic-angular';
 
 @Component({
   selector: 'page-home',
@@ -7,8 +8,39 @@ import { NavController } from 'ionic-angular';
 })
 export class HomePage {
 
-  constructor(public navCtrl: NavController) {
+  registerForm: FormGroup;
 
+  constructor(
+    public navCtrl: NavController,
+    private formBuilder: FormBuilder,
+    public alertCtrl: AlertController
+  ) {
+    this.buildForm();
+  }
+
+  doSave() {
+    console.log(JSON.stringify(this.registerForm.value));
+    let alert = this.alertCtrl.create({
+      title: '¡Genial!',
+      subTitle: "Registraste con exito a " + this.registerForm.value['name'],
+      buttons: ['OK']
+    });
+    alert.present();
+    this.registerForm.setValue({name:'',country:'',recommend:null});
+  }
+
+  private buildForm() {
+    this.registerForm = this.formBuilder.group({
+      name: ['', [Validators.required, Validators.maxLength(15)]],
+      country: ['', [Validators.required, Validators.maxLength(12), Validators.minLength(3)]],
+      recommend: ['', Validators.required],
+      /*check: false,
+      date: '',
+      peso: 0,
+      banda: '',
+      bandas: '',
+      notifications: false*/
+    });
   }
 
 }
